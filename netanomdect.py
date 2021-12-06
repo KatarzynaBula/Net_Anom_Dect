b225965df88c20820608cd316b4f921728e74104
KatarzynaBula <79314435+KatarzynaBula@users.noreply.github.com>
2021-12-06 22:48:25 +0100

    edit

--- netanomdect.py
+++ netanomdect.py
@@ -1,3 +1,35 @@
-from pylibpcap.pcap import rpcap
-import socket
-import struct
+from scapy.all import *
+
+file = sys.argv[1]
+
+n = len(sys.argv)
+print("Total arguments passed:", n)
+
+
+def checkmac(mac, ip):
+    if not mac in legitmaclist:
+        print("Warning! Suspicious mac adress was detected:", mac,"  ", ip)
+
+packets = rdpcap('/home/robert/pcap/t3.pcap')
+
+legitmaclist = []
+with open("legitmacaddressess.txt") as file:
+    legitmacmaclist = file.readlines()
+
+
+
+for packet in packets:
+    if IP in packet:
+        ipsource=packet[IP].src
+        ipdestiation = packet[IP].dst
+    e = packet[Ether]
+    macsource = e.src
+    macdestination =e.dst
+    checkmac(macsource, ipsource)
+    checkmac(macdestination, ipdestiation)
+   
+
+    #print(ipsource, ipdestiation, macsource, macdestination)
+    #print(macsource, macdestination, sep='\n')
+
+
